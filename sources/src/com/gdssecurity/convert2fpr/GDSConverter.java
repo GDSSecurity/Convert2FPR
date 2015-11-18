cb6c53142ad422fbc4e3e8ae920b75ead96125cd
Ahmed Mahmoud <amahmoud@gdssecurity.com>
2015-11-18 10:20:52 -0500

    Add author info and blogpost

--- sources/src/com/gdssecurity/convert2fpr/GDSConverter.java
+++ sources/src/com/gdssecurity/convert2fpr/GDSConverter.java
@@ -1,6 +1,8 @@
 /*
  *  Gotham Digital Science LLC
  *  @author Andrea Scaduto
+ *  @Email ascaduto@gdssecurity.com
+ *  @Blogpost https://blog.gdssecurity.com/labs/2015/6/13/converting-findbugs-xml-to-hp-fortify-sca-fpr.html
   / ___|___  _ ____   _____ _ __| |_|___ \|  ___|  _ \|  _ \ 
  | |   / _ \| '_ \ \ / / _ \ '__| __| __) | |_  | |_) | |_) |
  | |__| (_) | | | \ V /  __/ |  | |_ / __/|  _| |  __/|  _ < 
